f790cf7e2130ae24d1723696653155397868001d
octolss <147706639+octolss@users.noreply.github.com>
2024-02-02 08:45:11 +0000

    .

---
+++ Vova.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+class User {
+public:
+string login;
+string password;
+};
+
+class Clothes {
+public:
+string brand;
+string type;
+int amount;
+string gender; 
+
+
+Clothes(string brand, string type, int amount, string gender)
+: brand(brand), type(type), amount(amount), gender(gender) {}
+
+
+virtual void displayInfo() {
+cout << "Brand: " << brand << ", Type: " << type << ", Amount: " << amount << ", Gender: " << gender << endl;
+}
+};
+
+class Admin : public User {
+public:
+vector<User*> bannedUsers;
+vector<Clothes*> items;
+};
+
+
+
+
+class Review {
+public:
+string reviewText;
+string login;
+int rating;
+};
+
+
+class Cart {
+public:
+int itemCount;
+int totalPrice;
+};
+
+
+class Payment : public Cart {
+public:
+int totalPrice;
+string paymentMethod;
+};
+
+
+class Jeans : public Clothes {
+public:
+int size;
+string material;
+string color;
+
+Jeans(string brand, string type, int amount, string gender, int size, string material, string color)
+: Clothes(brand, type, amount, gender), size(size), material(material), color(color) {}
+
+
+void displayInfo() override {
+cout << "Jeans - Size: " << size << ", Material: " << material << ", Color: " << color << endl;
+}
+};
+
+
+
+int main() {
+
+Jeans myJeans("Levi's", "Denim", 50, "man", 32, "Denim", "Blue");
+myJeans.displayInfo();
+
+
+return 0;
+}
